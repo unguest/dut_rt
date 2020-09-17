5f1ab0b0d2807345a3ad1e78a05c1f75309c1cc1
Arthur Pons <unguestdev@gmail.com>
2020-09-17 13:01:52 +0000

    Create trier_albert.sh

---
+++ RTAM1105/trier_albert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A executer dans le repertoire parent de fichiers_TP1 qui doit etre
+# le meme que celui de TP_initiallisation
+
+# Traitement des fichiers lies aux vacances :
+mv fichiers_TP1/photo* TP_initiation/Albert/Vacances/Photos
+mv fichiers_TP1/video* TP_initiation/Albert/Vacances/Videos
+
+# Traitement des fichiers lies au travail :
+mv fichiers_TP1/cours* TP_initiation/Albert/Travail/Cours
+mv fichiers_TP1/TD* TP_initiation/Albert/Travail/TP
+mv fichiers_TP1/TP* TP_initiation/Albert/Travail/TP
