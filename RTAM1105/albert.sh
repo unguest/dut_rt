c33256c2d6c1ac18fe3d35cb594c36ebadaa1373
Arthur Pons <unguestdev@gmail.com>
2020-09-17 12:12:56 +0000

    Update albert.sh
    Fixing option error

--- RTAM1105/albert.sh
+++ RTAM1105/albert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -r TP_initiation/Albert/Vacances/Photos
+mkdir -p TP_initiation/Albert/Vacances/Photos
 mkdir TP_initiation/Albert/Vacances/Videos
 
-mkdir -r TP_initiation/Albert/Travail/TP
+mkdir -p TP_initiation/Albert/Travail/TP
 mkdir TP_initiation/Albert/Travail/Cours
