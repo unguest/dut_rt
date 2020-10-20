118573a57e64b8e70c42653efb438c9225526fac
Arthur Pons <unguestdev@gmail.com>
2020-10-20 15:16:03 +0200

    Update Q6.sh

--- RTAM1105/TP4/Q6.sh
+++ RTAM1105/TP4/Q6.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# UNFINISHED SCRIPT
+
+# EQUIVALENT TO Q2.SH // BASED ON Q1
+
+max=$1
+
 for arg in "$@"
 do
-
-	echo $arg
-
+    if [ "$arg" -gt "$max" ] 
+    then
+        max=$arg 
+    fi
 done
+echo $max
