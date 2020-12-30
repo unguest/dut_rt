3c95ec0c9d61ad2a5fc20224bd8dba87af43cf1d
Arthur Pons <unguestdev@gmail.com>
2020-12-30 12:32:58 +0100

    Form handling  + minor css fix.
    Adding a file parser to the admin page so it can read the messages.txt file and correcting email display bug.

--- RTAM1206/EV1/admin.php
+++ RTAM1206/EV1/admin.php
@@ -1,3 +1,46 @@
-<?php
-echo("HELLO");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arthur Pons</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="particles-js"></div>
+
+    <menu>
+        <a href="index.html"><p>Home</p></a>
+        <a href="cv.html"><p>CV</p></a>
+        <a href="hobbies.html"><p>Hobbies</p></a>
+        <a href="contact.html"><p>Contact</p></a>
+        <a href="admin.php"><p>Admin</p></a>
+    </menu>
+
+    <main>        
+        <h1>Page d'administration</h1>
+
+        <p>Normalement, cette page est cachée mais pour les besoins du TP, elle est laissée en accès libre.</p>
+
+
+        <?php
+
+            if ($file = fopen("messages.txt", "r")) {
+                while(!feof($file)) {
+                    $textperline = fgets($file);
+                    echo "<p>$textperline</p>";
+                }
+                fclose($file);
+            } else {
+                echo "<p>Aucun commentaire pour le moment...</p>";
+            }
+        ?>
+
+
+    </main>
+
+    <script src="js/particles.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+</html>
--- RTAM1206/EV1/message.php
+++ RTAM1206/EV1/message.php
@@ -1,7 +1,7 @@
 <?php
 
     if (isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["email"]) && isset($_POST["message"])) {
-        $message = $_POST["name"] . $_POST["surname"] . "<".$_POST["email"]."> :" . $_POST["message"];
+        $message = $_POST["name"] . " " . $_POST["surname"] . "  [".$_POST["email"]."] : " . $_POST["message"] . PHP_EOL;
         $myfile = fopen("messages.txt", "a");
         fwrite($myfile, $message);
         fclose($myfile);
